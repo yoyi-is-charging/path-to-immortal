ce9765a80bdb83d9756365a97ee9eafa5d9713ac
yoyi-is-charging <195854665+yoyi-is-charging@users.noreply.github.com>
2025-07-08 01:32:14 +0800

    修复时间相关问题
    1. 修复可能导致解析时间为1天后的问题

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -5,7 +5,7 @@ export function min(a: Date, b: Date): Date {
 export function parseDate(input: string, pattern: RegExp): Date | undefined {
     const match = input.match(pattern);
     if (!match) return undefined;
-    const { hours, minutes = '0', seconds = '0', ms = '999' } = match.groups || {};
+    const { hours, minutes = '0', seconds = '0', ms = '1999' } = match.groups || {};
     if (hours)
         return getDate({ hours: parseInt(hours), minutes: parseInt(minutes), seconds: parseInt(seconds), ms: parseInt(ms) });
     else
@@ -13,10 +13,11 @@ export function parseDate(input: string, pattern: RegExp): Date | undefined {
 }
 export function getDate({ hours = 0, minutes = 0, seconds = 0, ms = 0, dayOffset = undefined }: { hours?: number; minutes?: number; seconds?: number; ms?: number; dayOffset?: number }): Date {
     const now = new Date();
+    const offset = 1 * 60 * 60 * 1000; // 1 hour in milliseconds
     const date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes, seconds, ms);
     if (typeof dayOffset === 'number')
         date.setDate(date.getDate() + dayOffset);
-    else if (date.getTime() < now.getTime())
+    else if (date.getTime() < now.getTime() - offset)
         date.setDate(date.getDate() + 1);
     return date;
 }
