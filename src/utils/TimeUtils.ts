23a8b72f6275318e16fcbf268caa82afce24f694
yoyi-is-charging <195854665+yoyi-is-charging@users.noreply.github.com>
2025-07-10 00:24:31 +0800

    修复进入鱼塘消息消失导致的错误

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -5,13 +5,13 @@ export function min(a: Date, b: Date): Date {
 export function parseDate(input: string, pattern: RegExp): Date | undefined {
     const match = input.match(pattern);
     if (!match) return undefined;
-    const { hours, minutes = '0', seconds = '0', ms = '1999' } = match.groups || {};
+    const { hours, minutes = '0', seconds = '0', ms = '2000' } = match.groups || {};
     if (hours)
         return getDate({ hours: parseInt(hours), minutes: parseInt(minutes), seconds: parseInt(seconds), ms: parseInt(ms) });
     else
         return new Date(Date.now() + 60 * 1000 * parseInt(minutes));
 }
-export function getDate({ hours = 0, minutes = 0, seconds = 0, ms = 0, dayOffset = undefined }: { hours?: number; minutes?: number; seconds?: number; ms?: number; dayOffset?: number }): Date {
+export function getDate({ hours = 0, minutes = 0, seconds = 0, ms = 2000, dayOffset = undefined }: { hours?: number; minutes?: number; seconds?: number; ms?: number; dayOffset?: number }): Date {
     const now = new Date();
     const offset = 1 * 60 * 60 * 1000; // 1 hour in milliseconds
     const date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes, seconds, ms);
