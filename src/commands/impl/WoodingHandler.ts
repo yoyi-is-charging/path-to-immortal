eb74def23af353fce0327c680a5d3eeeaab2a7de
yoyi-is-charging <195854665+yoyi-is-charging@users.noreply.github.com>
2025-08-21 18:13:52 +0800

    更新友商价格查询
    1. 更新友商价格查询
    2. 为宗门悬赏提供初始时刻设置
    3. 修复偶尔无法捕获发送命令参数的问题

--- src/commands/impl/WoodingHandler.ts
+++ src/commands/impl/WoodingHandler.ts
@@ -16,11 +16,16 @@ export default class WoodingHandler implements CommandHandler {
         ['我的树木', 'wooding_priceInquiry'],
         ['出售给木商', 'wooding_sell'],
         ['确定出售给木商', 'wooding_sell'],
+        ['友商报价', 'wooding_priceInquiryFriend'],
+        ['出售给友商', 'wooding_sellFriend'],
+        ['确定出售给友商', 'wooding_sellFriend'],
     ]);
     readonly RESPONSE_PATTERN = new Map([
         ['wooding', /已进入林场|已离开林场|无法进入林场|预计缺水时间|停止增长|砍伐完成|还未砍伐/],
-        ['wooding_priceInquiry', /我的木块|还没种过树/],
-        ['wooding_sell', /确定要出售|出售完成/],
+        ['wooding_priceInquiry', /门前木商报架|还没种过树/],
+        ['wooding_sell', /确定要出售给门前的木商|出售完成/],
+        ['wooding_priceInquiryFriend', /友商高报价/],
+        ['wooding_sellFriend', /确定要出售给友友的木商|出售完成/],
     ])
     readonly ENTER_PATTERN = /已进入林场|还未砍伐/;
     readonly WATER_TIME_PATTERN = /(?<hours>\d+)时(?<minutes>\d+)分(?<seconds>\d+)秒/;
@@ -29,6 +34,7 @@ export default class WoodingHandler implements CommandHandler {
     readonly WOOD_BLOCK_PATTERN = /我的木块:(?<amount>\d+)/;
     readonly PRICE_UPDATE_PATTERN = /(?<minutes>\d+)分后更新/;
     readonly CONFIRM_SELL_PATTERN = /确定要出售/;
+    readonly FRIEND_PRICE_PATTERN_GLOBAL = /(?<id>\d+):(?<price>\d+)\((?<duration>\d+)分后失效\)/g;
     readonly AMOUNT_THRESHOLD = 100;
 
     async handleResponse(command: Command, response: string, instance: GameInstance) {
@@ -61,14 +67,47 @@ export default class WoodingHandler implements CommandHandler {
             if (config.minPrice && price >= config.minPrice) {
                 if (process.env.BOT_CHAT_ID)
                     EventBus.emit('notification', { chatId: process.env.BOT_CHAT_ID, message: `木块价格已达到 ${price}，请及时出售` });
-                if (amount > this.AMOUNT_THRESHOLD)
+                if (amount >= this.AMOUNT_THRESHOLD)
                     instance.scheduleCommand({ type: 'wooding_sell', body: `出售给木商 ${amount}` });
+            }
+        }
+        if (command.type === 'wooding_priceInquiryFriend') {
+            const friendPrices = [...response.matchAll(this.FRIEND_PRICE_PATTERN_GLOBAL)];
+            const amount = parseInt(response.match(this.WOOD_BLOCK_PATTERN)!.groups!.amount);
+            const index = friendPrices.reduce((maxIndex, current, i) => {
+                const duration = parseInt(current.groups!.duration);
+                if (duration > 0 && (maxIndex == -1 || parseInt(current.groups!.price) > parseInt(friendPrices[maxIndex].groups!.price))) {
+                    return i;
+                }
+                return maxIndex;
+            }, -1);
+
+            if (index >= 0) {
+                const { id, price } = friendPrices[index].groups!;
+                if (config.minPrice && parseInt(price) >= config.minPrice) {
+                    const pattern = new RegExp(`友${id}.*出售给友商 数量 (?<reference>\\d+)0`);
+                    const reference = response.match(pattern)!.groups!.reference;
+                    if (process.env.BOT_CHAT_ID)
+                        EventBus.emit('notification', { chatId: process.env.BOT_CHAT_ID, message: `友商${id} (uid:${reference}) 的木块报价已达到 ${price}，请及时出售` });
+                    if (amount >= this.AMOUNT_THRESHOLD)
+                        instance.scheduleCommand({ type: 'wooding_sellFriend', body: `出售给友商 ${amount} ${reference}` });
+                }
+            }
+            if (config.friendPriceInquiryInterval) {
+                const friendPricesUpdateTime = new Date(Date.now() + config.friendPriceInquiryInterval * 60 * 1000);
+                instance.updateStatus({ wooding: { amount, friendPricesUpdateTime } });
+                instance.scheduleCommand({ type: 'wooding_priceInquiryFriend', body: '友商报价', date: friendPricesUpdateTime });
             }
         }
         if (command.type === 'wooding_sell') {
             const isSelling = this.CONFIRM_SELL_PATTERN.test(response);
             if (isSelling)
                 instance.scheduleCommand({ type: 'wooding_sell', body: '确定出售给木商' });
+        }
+        if (command.type === 'wooding_sellFriend') {
+            const isSelling = this.CONFIRM_SELL_PATTERN.test(response);
+            if (isSelling)
+                instance.scheduleCommand({ type: 'wooding_sellFriend', body: '确定出售给友商' });
         }
     }
 
@@ -78,7 +117,7 @@ export default class WoodingHandler implements CommandHandler {
     }
 
     registerScheduler(instance: GameInstance): void {
-        ['wooding', 'wooding_priceInquiry'].forEach(type => this.registerTypeScheduler(instance, type));
+        ['wooding', 'wooding_priceInquiry', 'wooding_priceInquiryFriend'].forEach(type => this.registerTypeScheduler(instance, type));
     }
 
     registerTypeScheduler(instance: GameInstance, type: string): void {
@@ -100,6 +139,8 @@ export default class WoodingHandler implements CommandHandler {
             case 'wooding_priceInquiry':
                 instance.scheduleCommand({ type: 'wooding_priceInquiry', body: '我的树木', date: status?.priceUpdateTime });
                 break;
+            case 'wooding_priceInquiryFriend':
+                instance.scheduleCommand({ type: 'wooding_priceInquiryFriend', body: '友商报价', date: status?.friendPricesUpdateTime });
         }
     }
 }
