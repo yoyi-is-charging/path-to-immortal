40a9ee4c9b2c490b7955e3b49fd820455d74d5f3
yoyi-is-charging <195854665+yoyi-is-charging@users.noreply.github.com>
2025-09-13 23:22:11 +0800

    增强报错输出

--- src/server/core/EventBus.ts
+++ src/server/core/EventBus.ts
@@ -10,7 +10,7 @@ export class EventBus {
         EventBus.on('commandScheduled', ({ accountId, command }: { accountId: string, command: Command }) => logger.debug(`Command scheduled for account ${accountId}: ${command.type} at ${new Date(command.date!).toLocaleString()}`));
         EventBus.on('commandSent', ({ accountId, command }: { accountId: string, command: Command }) => logger.debug(`Command sent for account ${accountId}: ${command.type}`));
         EventBus.on('commandProcessed', ({ accountId, command }: { accountId: string, command: Command }) => logger.debug(`Command processed for account ${accountId}: ${command.type}`));
-        EventBus.on('commandFailed', ({ accountId, command, error }: { accountId: string, command: Command, error: string }) => logger.info(`Command failed for account ${accountId}: ${command.type} - ${error}`));
+        EventBus.on('commandFailed', ({ accountId, command, error, response }: { accountId: string, command: Command, error: string, response: string }) => logger.info(`Command failed for account ${accountId}: ${command.type} - ${error} - ${response}`));
         EventBus.on('processCommandError', ({ accountId, command, error }: { accountId: string, command: Command, error: string }) => logger.error(`Error processing command ${command.type} for account ${accountId}: ${error}`));
         EventBus.on('sessionUpdateScheduled', ({ accountId, timestamp }: { accountId: string, timestamp: number }) => logger.info(`Relogin scheduled for account ${accountId} at ${new Date(timestamp).toLocaleString()}`));
     }
